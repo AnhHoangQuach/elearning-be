e7810a3d1c6b66706e124ee7b9a98ae882e53a68
Quách Hoàng Anh <quachhoanganh@MBP-cua-Quach.net.fpt>
2024-02-04 22:05:58 +0700

    fix: role teachers

--- src/apis/adminUser.api.js
+++ src/apis/adminUser.api.js
@@ -1,38 +1,88 @@
-const express = require('express');
+const express = require('express')
 const adminUserApis = express.Router()
-const adminUserController = require('../controllers/adminUser.controller');
-const passport = require('../middlewares/passport.middleware');
-const { dontStorageUpload } = require('../configs/storage.config');
-
+const adminUserController = require('../controllers/adminUser.controller')
+const passport = require('../middlewares/passport.middleware')
+const { dontStorageUpload } = require('../configs/storage.config')
 
 // api: lấy danh sách tài khoản giáo viên
-adminUserApis.get('/teachers', passport.jwtAuthentication, passport.isAdmin, adminUserController.getTeachers)
+adminUserApis.get(
+  '/teachers',
+  passport.jwtAuthentication,
+  passport.isAdmin,
+  adminUserController.getTeachers
+)
 
 // api: lấy chi tiết 1 tài khoản giáo viên bằng id
-adminUserApis.get('/teachers/:id', passport.jwtAuthentication, passport.isAdmin, adminUserController.getDetailTeacher)
+adminUserApis.get(
+  '/teachers/:id',
+  passport.jwtAuthentication,
+  passport.isAdmin,
+  adminUserController.getDetailTeacher
+)
 
 // api: lấy danh sách tài khoản user
-adminUserApis.get('/', passport.jwtAuthentication, passport.isAdmin, adminUserController.getAccountAndUsers)
+adminUserApis.get(
+  '/',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  adminUserController.getAccountAndUsers
+)
 
 // api: lấy chi tiết 1 tài khoản user bằng id
-adminUserApis.get('/:id', passport.jwtAuthentication, passport.isAdmin, adminUserController.getDetailAccountAndUser)
+adminUserApis.get(
+  '/:id',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  adminUserController.getDetailAccountAndUser
+)
 
 // api: tạo một tài khoản và người dùng
-adminUserApis.post('/', passport.jwtAuthentication, passport.isAdmin, adminUserController.postAccountAndUser)
+adminUserApis.post(
+  '/',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  adminUserController.postAccountAndUser
+)
 
 // api: tạo nhiều tài khoản người dùng
-adminUserApis.post('/multiple', passport.jwtAuthentication, passport.isAdmin, dontStorageUpload.single('file'), adminUserController.postMultiAccountAndUser)
+adminUserApis.post(
+  '/multiple',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  dontStorageUpload.single('file'),
+  adminUserController.postMultiAccountAndUser
+)
 
 // api: cập nhật tài khoản người dùng
-adminUserApis.put('/:id', passport.jwtAuthentication, passport.isAdmin, adminUserController.putAccountAndUser)
+adminUserApis.put(
+  '/:id',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  adminUserController.putAccountAndUser
+)
 
 // api: xoá nhiều tài khoản người dùng
-adminUserApis.delete('/multiple', passport.jwtAuthentication, passport.isAdmin, adminUserController.deleteMultiAccountAndUser)
+adminUserApis.delete(
+  '/multiple',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  adminUserController.deleteMultiAccountAndUser
+)
 
 // api: xoá tài khoản người dùng
-adminUserApis.delete('/:id', passport.jwtAuthentication, passport.isAdmin, adminUserController.deleteAccountAndUser)
+adminUserApis.delete(
+  '/:id',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  adminUserController.deleteAccountAndUser
+)
 
 // api: lấy danh sách user đã mua khoá học của teacher
-adminUserApis.get('/students-of-teacher/:id', passport.jwtAuthentication, passport.isAdmin, adminUserController.getStudentsOfTeacher)
+adminUserApis.get(
+  '/students-of-teacher/:id',
+  passport.jwtAuthentication,
+  passport.isAdminOrTeacher,
+  adminUserController.getStudentsOfTeacher
+)
 
 module.exports = adminUserApis
