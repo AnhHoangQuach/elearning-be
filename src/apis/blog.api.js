51bca0f4097af6cc76f879a0973c6e3894163478
AnhHoangQuach <hoanganh36.work@gmail.com>
2024-03-15 00:38:28 +0700

    fix: role get post

--- src/apis/blog.api.js
+++ src/apis/blog.api.js
@@ -3,7 +3,7 @@ var blogApis = express.Router();
 const blogController = require('../controllers/blog.controller');
 const passport = require('../middlewares/passport.middleware');
 
-blogApis.get('/', passport.jwtAuthentication, passport.isAdminOrTeacher, blogController.fetchBlogs)
+blogApis.get('/', blogController.fetchBlogs)
 blogApis.post('/', passport.jwtAuthentication, passport.isAdminOrTeacher, blogController.createBlog)
 blogApis.delete('/:id', passport.jwtAuthentication, passport.isAdminOrTeacher, blogController.deleteBlog)
 
