22fa48fab2db8e54500ac750b3c3044c1d65a1b4
AnhHoangQuach <hoanganh36.work@gmail.com>
2024-03-15 00:06:47 +0700

    fix: check role admin or teacher

--- src/apis/blog.api.js
+++ src/apis/blog.api.js
@@ -3,8 +3,8 @@ var blogApis = express.Router();
 const blogController = require('../controllers/blog.controller');
 const passport = require('../middlewares/passport.middleware');
 
-blogApis.get('/', blogController.fetchBlogs)
-blogApis.post('/', passport.jwtAuthentication, passport.isAdmin, blogController.createBlog)
-blogApis.delete('/:id', passport.jwtAuthentication, passport.isAdmin, blogController.deleteBlog)
+blogApis.get('/', passport.jwtAuthentication, passport.isAdminOrTeacher, blogController.fetchBlogs)
+blogApis.post('/', passport.jwtAuthentication, passport.isAdminOrTeacher, blogController.createBlog)
+blogApis.delete('/:id', passport.jwtAuthentication, passport.isAdminOrTeacher, blogController.deleteBlog)
 
 module.exports = blogApis
